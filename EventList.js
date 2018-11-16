08986f4ea1f3f1e10d9237f52a1bf878e70d6f79
daervo <ervinmac@Ervins-MacBook-Pro.local>
2018-11-16 13:16:37 +1100

    working datepicker in form

--- EventList.js
+++ EventList.js
@@ -1,7 +1,16 @@
 import React, { Component }  from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, StyleSheet } from 'react-native';
+import ActionButton from 'react-native-action-button';
 
 import EventCard from './EventCard';
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        paddingTop: 10,
+        backgroundColor: '#F3F3F3'
+    }
+});
 
 class EventList extends React.Component{
     state = {
@@ -9,18 +18,40 @@ class EventList extends React.Component{
     }
 
     componentDidMount(){
-        const events = require('./db.json').events;
+        setInterval(() => {
+            this.setState({
+                events: this.state.events.map(evt => ({
+                    ...evt,
+                    timer: Date.now() // this prop is useless but tells component to rerender
+                }))
+            });
+        }, 1000);
+
+        const events = require('./db.json').events.map(e => ({
+            ...e,
+            date: new Date(e.date)
+        }));
         this.setState({ events });
     }
 
+    handleAddEvent = () => {
+        this.props.navigation.navigate('form');
+    }
+
     render(){
-        return(
+        return[
             <FlatList 
                 data={this.state.events}
+                style={styles.list}
                 renderItem={({ item }) => <EventCard event={item} />}
                 keyExtractor={item => item.id}
+            />,
+            <ActionButton
+                key="fab"
+                onPress={this.handleAddEvent}
+                buttonColor="rgba(231, 76, 60, 1)"
             />
-        );
+        ];
     }
 }
 
